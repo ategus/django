ea88f229260c8e16fa20b11b36b6a2aa4d53b4c6
ategus <ronny.schellenberg@gmail.com>
2019-04-12 20:03:30 +0200

    headline

--- leadmanager/frontend/src/components/App.js
+++ leadmanager/frontend/src/components/App.js
@@ -1,9 +1,14 @@
 import React, {Component} from 'react';
 import ReactDom from 'react-dom';
 
+import Header from './layout/Header'
+
 class App extends Component {
     render() {
-        return <h1>React App</h1>
+        //return <h1>React App</h1>
+        return (
+            <Header />
+        )
     }
 }
 
